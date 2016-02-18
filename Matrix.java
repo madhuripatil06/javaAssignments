9f45b8b78312c443d8e48d1aa25d312c6415c553
Madhuri Patil <madhuri@thoughtworks.com>
2016-02-18 19:41:09 +0530

    addede the function to add two matrices

--- Matrix.java
+++ Matrix.java
@@ -18,4 +18,26 @@ class Matrix{
 		return this.matrix[positionOfRow][positionOfColumn] == value;
 	}
 
+	public boolean isEqualTo(Matrix givenMatrix){
+		return Arrays.deepEquals(this.matrix,givenMatrix.matrix);
+	}
+
+
+	private boolean isOfSameSize(Matrix other){
+		return this.columns == other.columns && this.rows == other.rows;
+	}
+
+	public Matrix addWith(Matrix addend){
+		if(this.isOfSameSize(addend)){
+			Matrix resultedMatrix = new Matrix(this.rows , this.columns);
+			for(int i = 0 ; i < this.rows ;i++){
+				for(int j = 0 ; j < this.columns ;j++){
+					resultedMatrix.matrix[i][j] = this.matrix[i][j] + addend.matrix[i][j];
+				}
+			}
+			return resultedMatrix;
+		}
+		return new Matrix(0,0);
+	}
+
 };
--- MatrixTest.java
+++ MatrixTest.java
@@ -25,4 +25,37 @@ public class MatrixTest{
 		assertTrue(matrix1.isAtPosition(0,1,8));
 	}
 
+	@Test
+	public void add_matrices_of_3_by_3(){
+		Matrix addend1 = new Matrix(3,3);
+		Matrix addend2 = new Matrix(3,3);
+		for(int i = 0 ; i < 3 ;i++){
+			for(int j = 0 ;  j < 3 ; j++){
+				addend1.insertValue(i,j,(i+j));
+			}
+			addend2.insertValue(i,i,1);
+		}
+		Matrix sum = addend1.addWith(addend2);
+		Matrix sumShouldbe = new Matrix(3,3);
+		sumShouldbe.insertValue(0,0,1);
+		sumShouldbe.insertValue(0,1,1);
+		sumShouldbe.insertValue(0,2,2);
+		sumShouldbe.insertValue(1,0,1);
+		sumShouldbe.insertValue(1,1,3);
+		sumShouldbe.insertValue(1,2,3);
+		sumShouldbe.insertValue(2,0,2);
+		sumShouldbe.insertValue(2,1,3);
+		sumShouldbe.insertValue(2,2,5);
+		assertTrue(sum.isEqualTo(sumShouldbe));
+ 	}
+
+ 	@Test
+	public void add_matrix_of_different_sizes(){
+		Matrix addend1 = new Matrix(0,3);
+		Matrix addend2 = new Matrix(3,9);
+		Matrix sum = addend1.addWith(addend2);
+		Matrix sumShouldbe = new Matrix(0,0);
+		assertTrue(sum.isEqualTo(sumShouldbe));
+ 	}
+
 }
