062d05c6da86ef907be50e24b7a8b29008730c42
Madhuri Patil <madhuri@thoughtworks.com>
2016-02-18 19:39:04 +0530

    addede the insertValue function to add value to particular location

--- Matrix.java
+++ Matrix.java
@@ -1,12 +1,21 @@
+import java.util.Arrays;
 class Matrix{
-	private int Rows;
-	private int Colmns;
-	int [][] Matrix;
+	private int rows;
+	private int columns;
+	private int [][] matrix;
+	
 	public Matrix(int noOfRows , int noOfColmns){
-		this.Rows = noOfRows;
-		this.Colmns = noOfColmns;
-		this.Matrix = new int[noOfRows][noOfColmns];
+		this.rows = noOfRows;
+		this.columns = noOfColmns;
+		this.matrix = new int[noOfRows][noOfColmns];
 	};
 
-	
-}
+	public void insertValue(int positionOfRow , int positionOfColumn , int value){
+		this.matrix[positionOfRow][positionOfColumn] = value;
+	}
+
+	public boolean isAtPosition(int positionOfRow , int positionOfColumn ,int value){
+		return this.matrix[positionOfRow][positionOfColumn] == value;
+	}
+
+};
--- MatrixTest.java
+++ MatrixTest.java
@@ -1,14 +1,28 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 
 public class MatrixTest{
 	@Test
 	public void createMatrix(){
 		Matrix sample = new Matrix(3,3);
-		for(int i = 0 ; i < 3 ;i++){
-			for(int j = 0 ; j < 3 ; j++){
-				assertEquals(0,sample.Matrix[i][j]);
+		Matrix resultedMatrix = new  Matrix(3,3);
+		for(int i = 0 ; i < 3 ; i++){
+			for(int  j = 0 ; j< 3 ;j++){
+				resultedMatrix.insertValue(i,j,0);
 			}
 		}
+		assertTrue(sample.isEqualTo(resultedMatrix));
 	}
+
+	@Test
+	public void insertValue(){
+		Matrix matrix1 = new Matrix(2,2);
+		matrix1.insertValue(0,0,9);
+		matrix1.insertValue(0,1,8);
+		assertTrue(matrix1.isAtPosition(0,0,9));
+		assertTrue(matrix1.isAtPosition(0,1,8));
+	}
+
 }
